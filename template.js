df31300c1567aa76625d8ba6d560e628c0a932ac
NatthakitChaiklahan <95482355+NatthakitChaiklahan@users.noreply.github.com>
2023-02-04 02:10:07 +0700

    Test Agian

---
+++ template.js
@@ -0,0 +1,15 @@
+
+VANTA.NET({
+  el: "#page-header",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x1c94e8,
+  backgroundColor: 0xb0b31,
+  maxDistance: 16.00,
+  spacing: 13.00
+})
